2a2b48164a25378cb031e7494db860d3ac3b2b3f
Rob Sawyer <robksawyer@gmail.com>
2013-06-03 14:55:35 -0700

    Added proper files back

---
+++ Config/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The bitlyKey assigned to your bit.ly account. (http://bit.ly/a/account)
+ */
+define('bitlyKey', 'R_d32557c82d370f17a0743dfc615a8d87');
+
+/**
+ * The bitlyLogin assigned to your bit.ly account. (http://bit.ly/a/account)
+ */
+define('bitlyLogin' , 'o_mpuk839e4');
+
+/**
+ * The client_id assigned to your OAuth app. (http://bit.ly/a/account)
+ */
+define('bitly_clientid' , 'cc225bcb635d87d579ee9ee853899c62c34589ca');
+
+/**
+ * The client_secret assigned to your OAuth app. (http://bit.ly/a/account)
+ */
+define('bitly_secret' , '4136acffe20c7caac01a47461b01d489ccd5db05');
+
+?>

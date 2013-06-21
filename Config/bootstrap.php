0df565d4644bee8b09c372461485c45caca6b98e
Rob Sawyer <robksawyer@gmail.com>
2013-06-20 17:02:26 -0700

    Fixed a bug in config

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -3,16 +3,13 @@
  * Required configuration variables
  */
 $bitly = array(
-	'Bitly' => array(
-		'api' => 'http://api.bit.ly/v3/', //The URI of the standard bitly v3 API.
-		'oauth_api' => 'https://api-ssl.bit.ly/v3/', //The URI of the bitly OAuth endpoints.
-		'oauth_access_token_endpoint' => 'https://api-ssl.bit.ly/oauth/', //The URI for OAuth access token requests.
-		'key' => getenv('BITLY_KEY'),
-		'login' => getenv('BITLY_LOGIN'),
-		'client_id' => getenv('BITLY_CLIENT_ID'),
-		'client_secret' => getenv('BITLY_CLIENT_SECRET')
-	)
+	'api' => 'http://api.bit.ly/v3/', //The URI of the standard bitly v3 API.
+	'oauth_api' => 'https://api-ssl.bit.ly/v3/', //The URI of the bitly OAuth endpoints.
+	'oauth_access_token_endpoint' => 'https://api-ssl.bit.ly/oauth/', //The URI for OAuth access token requests.
+	'key' => getenv('BITLY_KEY'),
+	'login' => getenv('BITLY_LOGIN'),
+	'client_id' => getenv('BITLY_CLIENT_ID'),
+	'client_secret' => getenv('BITLY_CLIENT_SECRET')
 );
-
 Configure::write('Bitly', $bitly);
 ?>
--- Config/bootstrap.php.default.php
+++ Config/bootstrap.php.default.php
@@ -17,15 +17,13 @@
  * Required configuration variables
  */
 $bitly = array(
-	'Bitly' => array(
-		'api' => 'http://api.bit.ly/v3/', //The URI of the standard bitly v3 API.
-		'oauth_api' => 'https://api-ssl.bit.ly/v3/', //The URI of the bitly OAuth endpoints.
-		'oauth_access_token_endpoint' => 'https://api-ssl.bit.ly/oauth/', //The URI for OAuth access token requests.
-		'key' => getenv('BITLY_KEY'), //The bitlyKey assigned to your bit.ly account. (http://bit.ly/a/account)
-		'login' => getenv('BITLY_LOGIN'), //The bitlyLogin assigned to your bit.ly account. (http://bit.ly/a/account)
-		'client_id' => getenv('BITLY_CLIENT_ID'), //The client_id assigned to your OAuth app. (http://bit.ly/a/account)
-		'client_secret' => getenv('BITLY_CLIENT_SECRET') //The client_secret assigned to your OAuth app. (http://bit.ly/a/account)
-	)
+	'api' => 'http://api.bit.ly/v3/', //The URI of the standard bitly v3 API.
+	'oauth_api' => 'https://api-ssl.bit.ly/v3/', //The URI of the bitly OAuth endpoints.
+	'oauth_access_token_endpoint' => 'https://api-ssl.bit.ly/oauth/', //The URI for OAuth access token requests.
+	'key' => getenv('BITLY_KEY'), //The bitlyKey assigned to your bit.ly account. (http://bit.ly/a/account)
+	'login' => getenv('BITLY_LOGIN'), //The bitlyLogin assigned to your bit.ly account. (http://bit.ly/a/account)
+	'client_id' => getenv('BITLY_CLIENT_ID'), //The client_id assigned to your OAuth app. (http://bit.ly/a/account)
+	'client_secret' => getenv('BITLY_CLIENT_SECRET') //The client_secret assigned to your OAuth app. (http://bit.ly/a/account)
 );
 
 Configure::write('Bitly', $bitly);
